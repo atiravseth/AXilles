6ae82943e34b8b7fcfabdb63dd2207cc1011359f
atiravseth <atiravsethwork@gmail.com>
2026-02-01 06:59:35 +0530

    dashboard source files

---
+++ MRSD Tasks/Sensors Lab/arduino_dashboard/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'arduino_dashboard'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['plugin.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='your@email.com',
+    description='RQt dashboard for Arduino sensors and motors',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'serial_node = arduino_dashboard.serial_node:main',
+        ],
+    },
+)
